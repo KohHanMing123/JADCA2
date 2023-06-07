f2316fb8776a1985c7839c2d0847ad13d7fc0e05
Jerald Lee <jleeshancheng@gmail.com>
2023-06-08 01:30:14 +0800

    Testing Servlet

---
+++ JAD_CA1-master/src/main/java/profile/CheckProfileExistence.java
@@ -0,0 +1,46 @@
+package profile;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CheckProfileExistence
+ */
+@WebServlet("/CheckProfileExistence")
+public class CheckProfileExistence extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CheckProfileExistence() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if(request.getAttribute("token") == null) {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("Pages/Login.jsp");
+    		dispatcher.forward(request, response);
+    		
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
