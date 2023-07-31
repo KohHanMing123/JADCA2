c32d6bcd4de9319c821be69854e4a5daa7bfffd8
KohHanMing123 <kohhanming123@gmail.com>
2023-08-01 06:38:58 +0800

    paypal SDK added, payment classes added, some errors

---
+++ JAD_CA1-master/src/main/java/payment/PaymentServices.java
@@ -0,0 +1,53 @@
+package payment;
+
+
+import java.util.*;
+
+//import com.paypal.api.payments.*;
+//import com.paypal.base.rest.*;
+public class PaymentServices {
+	
+	//APP NAME - JADCA2
+//	private static final String CLIENT_ID = "AevVRCx23nTS41nZpfTHRuCbjSVY9GMqkKt_Kmr0cMhalHig5I5nBIUSSPpRsTWTHPu93waV265ON0nN";
+//    private static final String CLIENT_SECRET = "EAQs3Y0X6Eofhh5Mbh067oxrMVcsX_6ESJr1ZO1M4usVFee2b3ecc-cQVgElEvZv5_MQSK1vGjMcciRj";
+//    private static final String MODE = "sandbox";
+// 
+//    public String authorizePayment(OrderDetails orderDetail)        
+//            throws PayPalRESTException {       
+// 
+//        Payer payer = getPayerInformation();
+//        RedirectUrls redirectUrls = getRedirectURLs();
+//        List<Transaction> listTransaction = getTransactionInformation(orderDetail);
+//         
+//        Payment requestPayment = new Payment();
+//        requestPayment.setTransactions(listTransaction);
+//        requestPayment.setRedirectUrls(redirectUrls);
+//        requestPayment.setPayer(payer);
+//        requestPayment.setIntent("authorize");
+// 
+//        APIContext apiContext = new APIContext(CLIENT_ID, CLIENT_SECRET, MODE);
+// 
+//        Payment approvedPayment = requestPayment.create(apiContext);
+// 
+//        return getApprovalLink(approvedPayment);
+// 
+//    }
+//     
+//    private Payer getPayerInformation() {
+//        ...
+//    }
+//     
+//    private RedirectUrls getRedirectURLs() {
+//
+//    }
+//     
+//    private List<Transaction> getTransactionInformation(OrderDetails orderDetail) {
+//    }
+//     
+//    private String getApprovalLink(Payment approvedPayment) {
+//		return null;
+// 
+//    }
+    
+    
+}
